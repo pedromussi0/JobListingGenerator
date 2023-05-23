98828649c59a7719b03434e885a098563dd49d1f
Pedro Mussi <phpmussi@gmail.com>
2023-05-23 15:19:36 -0300

    update

--- JobListingsProject/settings.py
+++ JobListingsProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +74,14 @@ WSGI_APPLICATION = 'JobListingsProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'job_listings_db',
-        'USER': 'postgres',
-        'PASSWORD': '250591',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
         'HOST': 'db',
         'PORT': '5432',
     }
